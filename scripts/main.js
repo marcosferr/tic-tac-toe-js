a03423039e5ca4dbd0a22015e7f0f4cc98087f5a
marcosferr <ferreiramarcosadrian@gmail.com>
2023-06-23 14:20:10 -0400

    add logic to handle basic gameplay

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,79 @@
 const gameBoard = (function gameBoard() {
-    const board = [['X', 'X', 'X'], ['X', 'X', 'X'], ['X', 'X', 'X']]
-    function setBox(number, value) {
-        let line = Math.floor((number - 1) / 3);
-        let column = (number - 1) % 3;
-        board[line][column] = value
-    }
-    function getBox(number) {
-        let line = Math.floor((number - 1) / 3);
-        let column = (number - 1) % 3;
-        v
-        return board[line][column]
-    }
-    return { setBox, getBox }
-})()
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  function setBox(number, value) {
+    let line = Math.floor((number - 1) / 3);
+    let column = (number - 1) % 3;
+    board[line][column] = value;
+  }
+  function getBox(number) {
+    let line = Math.floor((number - 1) / 3);
+    let column = (number - 1) % 3;
+
+    return board[line][column];
+  }
+  function emptyBoxes() {
+    let plano = board.flat();
+    const emptyIndexes = plano.reduce((acc, currentValue, index) => {
+      if (currentValue === "") {
+        acc.push(index + 1);
+      }
+      return acc;
+    }, []);
+    console.log(emptyIndexes);
+    return emptyIndexes;
+  }
+
+  return { setBox, getBox, emptyBoxes };
+})();
 
 const display = (function () {
-    let board = document.querySelectorAll('.square > div')
+  let board = document.querySelectorAll(".square > div");
 
-    function updateBoard() {
-        for (let i = 1; i <= board.length; i++) {
-
-
-            board[i - 1].innerHTML = gameBoard.getBox(i)
-
-        }
+  function updateBoard() {
+    for (let i = 1; i <= board.length; i++) {
+      board[i - 1].innerHTML = gameBoard.getBox(i);
     }
-    return { updateBoard }
-})()
+  }
+  return { updateBoard, board };
+})();
 
 const Player = (name, marker) => {
-    this.name = name
-    this.marker = marker
-}
+  return { name, marker };
+};
 
+const GameController = (function () {
+  const player = Player("Player 1", "X");
+  const computer = Player("Computer", "O");
+  let posiblesJugadas = [];
+  let turnoJugador = true;
 
+  function play(element) {
+    if (turnoJugador) {
+      gameBoard.setBox(element.getAttribute("number"), player.marker);
+      turnoJugador = false;
+      computerPlay();
+    }
+    display.updateBoard();
+  }
+  function computerPlay() {
+    posiblesJugadas = gameBoard.emptyBoxes();
+    if (posiblesJugadas.length === 0) {
+      console.log("No hay mas casillas libres");
+    }
+    gameBoard.setBox(
+      posiblesJugadas[Math.floor(Math.random() * posiblesJugadas.length)],
+      computer.marker
+    );
+    turnoJugador = true;
+  }
 
+  return { play };
+})();
 
-
-
+display.board.forEach((el) =>
+  el.addEventListener("click", (evento) => GameController.play(evento.target))
+);
